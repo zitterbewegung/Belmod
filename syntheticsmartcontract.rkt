18b3700d0f09b055bf656723603e8754cfe6489a
Joshua Herman <zitterbewegung@gmail.com>
2018-01-02 06:42:48 -0600

    Getting data from external ipython notebook.

---
+++ syntheticsmartcontract.rkt
@@ -0,0 +1,82 @@
+#lang rosette
+
+(require javascript
+         csv-reading
+         rosette/lib/synthax)
+
+(define-symbolic a b boolean?)
+(define-symbolic x y integer?)
+
+(define sol
+    (solve (begin (assert a)
+                  (assert (= x 1))
+                  (assert (= y 2)))))
+(define sol-fail
+    (solve (begin (assert a)
+                  (assert (equal?  a y))
+                  (assert (= x 1))
+                  (assert (= y 2)))))
+(define variable (evaluate 4 sol))
+
+; Model Variable -> Pyramid-Smart-Contract
+; creates a variable that
+; y pixels from the top and 10 from the left 
+(define (make-contract model variable)
+  (if (sat? model)
+     `(begin (define x1 ,variable))
+     '(begin self-destruct)))
+
+(define (make-contract-javascript model variable)
+  (if (sat? model)
+     `(begin  (fprintf (current-output-port)
+           "var x=~a ;"
+           ,variable))
+     '(begin self-destruct)))
+
+;Model Variable
+(define (conditional-template boolean)
+  (let ((x (if boolean 'red 'green)))
+  (cond ((eq? x 'red)   (make-contract sol 1))
+        ((eq? x 'green) (make-contract sol -2))
+        (else          '(begin self-destruct)))))
+
+
+(define optimize-sol
+  (optimize #:maximize (list (+ x y))
+            #:guarantee (assert (< (- y x) 1))))
+
+(define input "809,10497.55071817646,14429.437428824125\n810,10504.662222465608,14416.385521201237\n811,10473.934629975964,14445.35613558157\n812,10344.797889171332,14367.442637747907\n813,10292.691542759238,14294.650528545799\n")
+(define next-row (make-csv-reader input))
+(next-row)
+(define (behavior-delta threshold)
+    (if (> threshold (string->number (cadr (next-row))))
+        (conditional-template #t)
+        (conditional-template #f)))
+(define output '(("" "ds" "yhat" "yhat_lower" "yhat_upper")
+  ("809"
+   "2018-01-05 21:00:00"
+   "12443.0934730835"
+   "10497.55071817646"
+   "14429.437428824125")
+  ("810"
+   "2018-01-05 22:00:00"
+   "12461.01925136911"
+   "10504.662222465608"
+   "14416.385521201237")
+  ("811"
+   "2018-01-05 23:00:00"
+   "12442.784072308054"
+   "10473.934629975964"
+   "14445.35613558157")
+  ("812"
+   "2018-01-06 00:00:00"
+   "12387.559937091984"
+   "10344.797889171332"
+   "14367.442637747907")
+  ("813"
+   "2018-01-06 01:00:00"
+   "12313.81510242808"
+   "10292.691542759238"
+   "14294.650528545799")))
+
+
